b7ec3b4c9c5ad6d06788f1814f58f207bba6351a
nmthong226 <nmthong226@gmail.com>
2025-04-26 21:50:04 +0700

    chore: implement basic be base

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,13 +1,26 @@
-const express = require('express');
+import express, { json } from 'express';
+import cors from "cors";
+import connectDB from './db.js';
+import dotenv from 'dotenv';
+
+//Routes
+import authRoutes from "./routes/auth.routes.js";
+
+dotenv.config();
+
 const app = express();
 const PORT = process.env.PORT || 3000;
 
-app.use(express.json());
+app.use(cors());
+app.use(json());
 
-// Example route
+connectDB();
+
 app.get('/', (req, res) => {
     res.send('Hello from Express!');
 });
+
+app.use("/api/auth", authRoutes);
 
 app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);

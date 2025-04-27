d6a0d3918751bf750d3fc829083f5703ba5cc374
nmthong226 <nmthong226@gmail.com>
2025-04-27 21:09:33 +0700

    style: update layout

--- client/src/layouts/MainLayout.tsx
+++ client/src/layouts/MainLayout.tsx
@@ -1,10 +1,14 @@
-import Sidebar from "@/components/sidebar";
+import Header from "@/components/header";
+import Sidebar from "@/components/sidebar/sidebar";
 
 
 const MainLayout = ({ children }: { children: React.ReactNode }) => (
     <div className="flex w-full h-screen">
         <Sidebar />
-        <main className="flex-1">{children}</main>
+        <main className="flex-1">
+            <Header />
+            {children}
+        </main>
     </div>
 );
 export default MainLayout;

4c05f4776a3be1f3de1577273c2489297b0e8d68
nmthong226 <nmthong226@gmail.com>
2025-04-28 12:18:45 +0700

    fix: correct link

--- client/src/layouts/AuthLayout.tsx
+++ client/src/layouts/AuthLayout.tsx
@@ -8,7 +8,7 @@ const AuthLayout = ({ children }: { children: React.ReactNode }) => (
   <div className="relative flex justify-center w-full min-h-screen">
     {children}
     <div className="right-3 bottom-3 absolute flex justify-between items-center space-x-2 bg-gray-50 shadow-md px-1 rounded-3xl w-fit h-12">
-      <a href="https:/github.com/nmthong226" target="_blank" className="flex items-center space-x-2 bg-white hover:bg-gray-50 shadow px-6 rounded-3xl w-fit h-10 hover:cursor-pointer">
+      <a href="https://github.com/nmthong226" target="_blank" className="flex items-center space-x-2 bg-white hover:bg-gray-50 shadow px-6 rounded-3xl w-fit h-10 hover:cursor-pointer">
         <Avatar className="size-8">
           <AvatarImage src="https://github.com/nmthong226.png" />
           <AvatarFallback>MT</AvatarFallback>
